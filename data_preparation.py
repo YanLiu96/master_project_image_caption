c75d2dafe5be6ace8df7bce8522bf45f4a184771
Yan Liu <278899085@qq.com>
2020-06-15 16:23:56 +0100

    dowload coco dataset

---
+++ data_preparation.py
@@ -0,0 +1,12 @@
+# data_preparation.py created in 2020.6.15
+
+# This file is used for preparating necessary data.
+# 1. coco train2014 dataset called train2014.zip and unzip it.
+# 2. the annotations(captions) for train2014.
+
+import os
+import time
+import json
+
+
+
