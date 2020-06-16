54983c90b0b5cc9885f2c533862610bc879b397e
Yan Liu <278899085@qq.com>
2020-06-16 07:59:24 +0100

    download necessary data: coco images and captions

--- data_preparation.py
+++ data_preparation.py
@@ -5,8 +5,26 @@
 # 2. the annotations(captions) for train2014.
 
 import os
-import time
-import json
+
+# Download image files
+image_folder = '/coco_dataset/train2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+    os.system('wget http://images.cocodataset.org/zips/train2014.zip')
+    os.system('unzip train2014.zip')
+    os.remove('train2014.zip')
+    print("coco train2014 images have been downloaded")
+else:
+    print("coco train2014 images have already exist")
+
+# Download annotation desgined by Andrej
+annotation_folder = '/coco_dataset/caption_datasets/'
+if not os.path.exists(os.path.abspath('.') + annotation_folder):
+    os.system('wget http://cs.stanford.edu/people/karpathy/deepimagesent/caption_datasets.zip')
+    os.system('unzip caption_datasets.zip')
+    os.remove('caption_datasets.zip')
+    print("captions have been downloaded")
+else:
+    print("captions have already exist")
 
 
 
