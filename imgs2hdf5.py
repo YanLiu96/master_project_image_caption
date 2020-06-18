8b5ecf14660e535073dd541c183a685c3e6e2797
Yan Liu <278899085@qq.com>
2020-06-18 11:26:45 +0100

    add time to show the process of loading images into hdf5

--- imgs2hdf5.py
+++ imgs2hdf5.py
@@ -1,4 +1,5 @@
 import os
+import time
 import h5py
 import json
 import numpy as np
@@ -26,12 +27,12 @@ def images_save_in_hdf5():
 
 # http://docs.h5py.org/en/stable/quick.html
 def create_hdf5_file(type, imgaes_names, image_captions):
-    
+    start = time.time()
     with h5py.File(os.path.join('saved_data/hdf5_images', type + '_images.hdf5'), 'a') as f:
         f.attrs['captions_per_image'] = 5
         images = f.create_dataset('images', (len(imgaes_names), 3, 256, 256), dtype='uint8')
         print("%s images storage in progress\n" % type)
-        # bar = Bar('Processing', max=20)
+        bar = Bar('Processing', max=len(imgaes_names))
         for i, path in enumerate(imgaes_names):
             img = imageio.imread(path)
             #img = imread(path)
@@ -44,8 +45,8 @@ def create_hdf5_file(type, imgaes_names, image_captions):
             img = img.transpose(2, 0, 1)
             assert img.shape == (3, 256, 256)
             assert np.max(img) <= 255
-
             # Save image to HDF5 file
             images[i] = img
-        #     bar.next()
-        # bar.finish()
+            bar.next() 
+        bar.finish()
+        print ('Time taken for loading {} images in hdf5 file: {} sec\n'.format(type, time.time() - start))
---
+++ test.py
@@ -0,0 +1,4 @@
+import time
+start = time.time()
+type="das"
+print ('Time taken for loading {} images in hdf5 file: {} sec\n'.format(type, time.time() - start))
