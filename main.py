199d2bf0bb9a73aba187c3c5e461ba617c437bfe
Yan Liu <278899085@qq.com>
2020-06-19 13:35:14 +0100

    Build the encoder and decodermodel

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from prepare.data_preprocess import split_dataset, build_vocabulary_word2idx, save_word2idx
-#from prepare import images_save_in_hdf5
+from prepare.imgs2hdf5 import images_save_in_hdf5
 from prepare.word2vec import save_cap_vec
 
 if __name__ == '__main__':
     split_dataset('coco_dataset/caption_datasets/dataset_coco.json', 50)
-    build_vocabulary_word2idx(5)
-    save_word2idx('saved_data/word2index')
+    #build_vocabulary_word2idx(5)
+    #save_word2idx('saved_data/word2index')
     #images_save_in_hdf5()
-    save_cap_vec(50)
+    #save_cap_vec(50)
