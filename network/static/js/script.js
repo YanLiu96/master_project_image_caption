756e2f97134415e3f610d15a22994eca02f49725
Yan Liu <278899085@qq.com>
2020-06-27 13:28:27 +0100

    js css for the index page

--- network/static/js/script.js
+++ network/static/js/script.js
@@ -1,142 +1,187 @@
-// File Upload
-// 
-function ekUpload(){
-    function Init() {
-  
-      console.log("Upload Initialised");
-  
-      var fileSelect    = document.getElementById('file-upload'),
-          fileDrag      = document.getElementById('file-drag'),
-          submitButton  = document.getElementById('submit-button');
-  
-      fileSelect.addEventListener('change', fileSelectHandler, false);
-  
-      // Is XHR2 available?
-      var xhr = new XMLHttpRequest();
-      if (xhr.upload) {
-        // File Drop
-        fileDrag.addEventListener('dragover', fileDragHover, false);
-        fileDrag.addEventListener('dragleave', fileDragHover, false);
-        fileDrag.addEventListener('drop', fileSelectHandler, false);
-      }
-    }
-  
-    function fileDragHover(e) {
-      var fileDrag = document.getElementById('file-drag');
-  
-      e.stopPropagation();
-      e.preventDefault();
-  
-      fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
-    }
-  
-    function fileSelectHandler(e) {
-      // Fetch FileList object
-      var files = e.target.files || e.dataTransfer.files;
-  
-      // Cancel event and hover styling
-      fileDragHover(e);
-  
-      // Process all File objects
-      for (var i = 0, f; f = files[i]; i++) {
-        parseFile(f);
-        uploadFile(f);
-      }
-    }
-  
-    // Output
-    function output(msg) {
-      // Response
-      var m = document.getElementById('messages');
-      m.innerHTML = msg;
-    }
-  
-    function parseFile(file) {
-  
-      console.log(file.name);
-      output(
-        '<strong>' + encodeURI(file.name) + '</strong>'
-      );
-      
-      // var fileType = file.type;
-      // console.log(fileType);
-      var imageName = file.name;
-  
-      var isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName);
-      if (isGood) {
-        document.getElementById('start').classList.add("hidden");
-        document.getElementById('response').classList.remove("hidden");
-        document.getElementById('notimage').classList.add("hidden");
-        // Thumbnail Preview
-        document.getElementById('file-image').classList.remove("hidden");
-        document.getElementById('file-image').src = URL.createObjectURL(file);
-      }
-      else {
-        document.getElementById('file-image').classList.add("hidden");
-        document.getElementById('notimage').classList.remove("hidden");
-        document.getElementById('start').classList.remove("hidden");
-        document.getElementById('response').classList.add("hidden");
-        document.getElementById("file-upload-form").reset();
-      }
-    }
-  
-    function setProgressMaxValue(e) {
-      var pBar = document.getElementById('file-progress');
-  
-      if (e.lengthComputable) {
-        pBar.max = e.total;
-      }
-    }
-  
-    function updateFileProgress(e) {
-      var pBar = document.getElementById('file-progress');
-  
-      if (e.lengthComputable) {
-        pBar.value = e.loaded;
-      }
-    }
-  
-    function uploadFile(file) {
-  
-      var xhr = new XMLHttpRequest(),
-        fileInput = document.getElementById('class-roster-file'),
-        pBar = document.getElementById('file-progress'),
-        fileSizeLimit = 1024; // In MB
-      if (xhr.upload) {
-        // Check if file is less than x MB
-        if (file.size <= fileSizeLimit * 1024 * 1024) {
-          // Progress bar
-          pBar.style.display = 'inline';
-          xhr.upload.addEventListener('loadstart', setProgressMaxValue, false);
-          xhr.upload.addEventListener('progress', updateFileProgress, false);
-  
-          // File received / failed
-          xhr.onreadystatechange = function(e) {
-            if (xhr.readyState == 4) {
-              // Everything is good!
-  
-              // progress.className = (xhr.status == 200 ? "success" : "failure");
-              // document.location.reload(true);
-            }
-          };
-  
-          // Start upload
-          xhr.open('POST', document.getElementById('file-upload-form').action, true);
-          xhr.setRequestHeader('X-File-Name', file.name);
-          xhr.setRequestHeader('X-File-Size', file.size);
-          xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-          xhr.send(file);
+/* If you are using webpack, do make sure to use import for jQuery, jQueryui and lodash */
+
+/* widget simulates upload using promises, the actual ajax function for upload the user has to write */
+
+(() => {
+  $.widget('wgt.imageUploader', {
+    _currentId: 0,
+    _$template: Handlebars.compile(`<li image-idx="{{imageid}}">
+          <img src="{{imageurl}}" />
+          <span class="image-remove fa fa-times-circle"></span>
+          <span class="image-size">{{imagesize}}</span>
+        </li>`),
+    _fileList: [],
+    _$fileCtrl: $('<input type="file" accept="image/*" multiple />'),
+
+    _calculateSize: sizeinBytes => {
+      return sizeinBytes >= 1048576 ? `${(sizeinBytes / 1048576).toFixed(2)} Mb` :
+        sizeinBytes >= 1024 ?
+        `${(sizeinBytes / 1024).toFixed(2)} Kb` :
+        `${sizeinBytes} bytes`;
+    },
+
+    _progressncount: function () {
+      let totalSize = _.sumBy(this._fileList, fl => fl.file.size);
+      let sizePercentage = Math.ceil(totalSize / this.options.maxSize * 100);
+      if (sizePercentage <= 100) {
+        if (sizePercentage <= 80) {
+          $('.progress-bar', $(this.element)).removeClass('bg-danger').addClass('bg-info').css({
+            width: `${sizePercentage}%`
+          });
         } else {
-          output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
+          $('.progress-bar', $(this.element)).removeClass('bg-info').addClass('bg-danger').css({
+            width: `${sizePercentage}%`
+          });
         }
+      } else {
+        $('.progress-bar', $(this.element)).removeClass('bg-info').addClass('bg-danger').css({
+          width: '100%'
+        });
       }
+
+      if (this._fileList.length > 0) {
+        $('.image-count', $(this.element)).html(this._fileList.length);
+        $('.image-tot-size', $(this.element)).html(this._calculateSize(totalSize));
+        $('.upload-start,.upload-clean', $(this.element)).removeAttr('disabled');
+      } else {
+        $('.image-count', $(this.element)).html('0');
+        $('.image-tot-size', $(this.element)).html('0 Mb');
+        $('.upload-start,.upload-clean', $(this.element)).attr('disabled', '');
+      }
+    },
+
+    _selectFiles: function (images) {
+      if (images.length > 0) {
+        let validFiles = _.filter(images, image => {
+          return image.type.match('image.*');
+        });
+        let currIndex = this._currentId;
+        this._fileList = this._fileList.concat(_.map(validFiles, vf => {
+          return {
+            imageid: this._currentId++,
+            file: vf
+          };
+        }));
+        _.forEach(validFiles, vf => {
+          let $html = $(this._$template({
+            imageurl: URL.createObjectURL(vf),
+            imagesize: this._calculateSize(vf.size),
+            imageid: currIndex++
+          }));
+
+          $('.image-remove', $html).click(e => {
+            $(e.target).parents('li').addClass('to-remove');
+            this._fileList = _.reject(this._fileList, f => {
+              return f.imageid == $(e.target).parents('li').attr('image-idx');
+            });
+            this._progressncount();
+            setTimeout(() => {
+              $(e.target).parents('li').remove();
+            }, 500);
+          });
+          this._progressncount();
+          $('.images-to-upload ul', $(this.element)).append($html);
+        });
+
+      }
+    },
+
+    options: {
+      maxSize: 26214400,
+      startUpload: files => {
+        console.log('Start uploading images');
+      }
+    },
+
+
+    _create: function () {
+      $('.image-upload', $(this.element)).click(e => {
+        this._$fileCtrl.click();
+        return false;
+      });
+
+      this._$fileCtrl.change(e => {//调用 _selectFiles方法
+        this._selectFiles($(e.target)[0].files);
+        $(e.target).val(null);
+      });
+
+      $('.max-size-limit', $(this.element)).html(`max size limit ${this._calculateSize(this.options.maxSize)}`);
+
+      $('.upload-start', $(this.element)).click(e => {
+        $('.images-to-upload li', $(this.element)).removeClass('upload-err').removeClass('upload-success');
+        this.options.startUpload(this._fileList);
+        return false;
+      });
+
+      $('.upload-clean', $(this.element)).click(e => {
+        this.clean();
+        return false;
+      });
+    },
+
+    clean: function () {
+      this._fileList = [];
+      $('.images-to-upload li', $(this.element)).remove();
+      this._progressncount();
+    },
+
+    updateStatus: function (errored) {
+      this._fileList = _.reject(this._fileList, f => {
+        return !_.some(errored, err => err.imageid == f.imageid);
+      });
+
+      _.each($('.images-to-upload li', $(this.element)), li => {
+        let $li = $(li);
+        if (_.some(this._fileList, f => f.imageid == $li.attr('image-idx'))) {
+          $li.addClass('upload-err');
+        } else {
+          $li.addClass('upload-success');
+        }
+
+        $('.upload-success', $(this.element)).remove();
+        this._progressncount();
+      });
+    },
+
+    _destroy: function () {}
+  });
+
+  // calling widget on div
+  $('#imageWgt').imageUploader({
+    startUpload: files => {
+      console.log('The number of uploaded images is: '+files.length)
+      // files 的格式为：
+      // Array (3)
+      // 0 {imageid: 0, file: File}
+      // 1 {imageid: 1, file: File}
+      // 2 {imageid: 2, file: File}
+
+      //console.log(files[0]['file']) file: File
+
+      var form_data = new FormData();
+      for (var x = 0; x < files.length; x++) {
+        form_data.append("images", files[x]['file']);
+      }
+
+      console.log('begin upload')
+      $.ajax({
+        type: 'POST',
+        url: '/upload',
+        dataType: 'json',
+        data: form_data,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function (data) {
+          console.log('Success!');
+        },
+      });
+
+      let errored = _.reject(files, f => f.imageid>=0);
+      setTimeout(() => {
+        $('#imageWgt').imageUploader('updateStatus', errored);
+      }, 2000);
     }
-  
-    // Check for the various File API support.
-    if (window.File && window.FileList && window.FileReader) {
-      Init();
-    } else {
-      document.getElementById('file-drag').style.display = 'none';
-    }
-  }
-  ekUpload();
+  });
+
+})();
