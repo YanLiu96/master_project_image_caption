3ad7e488a7970eca9acc9e2023985e9c834ff5d9
Yan Liu <278899085@qq.com>
2020-06-27 13:31:33 +0100

    Modify the way user start the project

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import argparse
+
 from prepare.data_preprocess import split_dataset, build_vocabulary_word2idx, save_word2idx
 from prepare.imgs2hdf5 import images_save_in_hdf5
 from prepare.word2vec import save_cap_vec
+from train.train import main_train
+from test.discrible import caption_img
 
 if __name__ == '__main__':
-    split_dataset('coco_dataset/caption_datasets/dataset_coco.json', 50)
-    #build_vocabulary_word2idx(5)
-    #save_word2idx('saved_data/word2index')
-    #images_save_in_hdf5()
+
+    parser = argparse.ArgumentParser(description='Yan Liu master project: let computer tell what the image is about')
+
+    parser.add_argument('--data_prepare', '-p', help='prepare the necessary data')
+    parser.add_argument('--train', '-tr', help='train the models')
+    parser.add_argument('--test', '-t', help='image path to be tested')
+    args = parser.parse_args()
+
+    # args = parser.parse_args()
+    if args.data_prepare:
+        split_dataset('coco_dataset/caption_datasets/dataset_coco.json', 50)
+        build_vocabulary_word2idx(5)
+        save_word2idx('saved_data/word2index')
+        #images_save_in_hdf5()
     #save_cap_vec(50)
+    elif args.train:
+        main_train()
+    elif args.test:
+        caption_img(args.test)
+    else:
+        print('Choose the things you want to do, you can use -h to get help')
