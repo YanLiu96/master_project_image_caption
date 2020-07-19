5af5764228d243142a4c2352cdfa93d17dedf489
Yan Liu <278899085@qq.com>
2020-07-19 13:16:01 +0100

    edit controler

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from prepare.imgs2hdf5 import images_save_in_hdf5
 from prepare.word2vec import save_cap_vec
 from train.train import main_train
 from test.discrible import caption_img
-
+from evaluate.evaluate import evaluate
 from subprocess import Popen, PIPE, CalledProcessError
 
 
@@ -14,18 +14,20 @@ if __name__ == '__main__':
 
     parser.add_argument('--control', '-c', help='control:data_prepare, train')
     parser.add_argument('--test', '-t', help='get the caption for the given image')
-    # parser.add_argument('--train', '-tr', help='train the models')
-    # parser.add_argument('--test', '-t', help='image path to be tested')
+    parser.add_argument('--evaluate', '-e', help='evaluate the test dataset')
+
     args = parser.parse_args()
     if args.control == 'data_prepare':
         split_dataset('coco_dataset/caption_datasets/dataset_coco.json', 50)
         build_vocabulary_word2idx(5)
         save_word2idx('saved_data/word2index')
-        #images_save_in_hdf5()
+        images_save_in_hdf5()
         save_cap_vec(50)
     elif args.control == 'train':
         main_train()
     elif args.test:
         caption_img(args.test)
+    elif args.evaluate:
+        evaluate(args.evaluate)
     else:
         print('Choose the things you want to do, you can use -h to get help')
